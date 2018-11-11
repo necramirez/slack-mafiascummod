031df99f4fc9919318d87a6497c1cb4d1d251973
Nikki Erwin Ramirez <necramirez@gmail.com>
2018-11-11 01:40:27 -0500

    Remove token handling because slash commands don't use them

--- api/slack.js
+++ api/slack.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const Game = require('../models/Game');
-const KnownToken = require('../models/KnownToken');
 
 const router = express.Router();
 
@@ -9,8 +8,6 @@ router.post('/event', (req, res) => {
     body: {
       challenge,
       event: { type: eventType },
-      team_id: teamId,
-      token,
       type,
     },
   } = req;
@@ -19,34 +16,10 @@ router.post('/event', (req, res) => {
       console.log('Verifying event request URL...');
       if (challenge) {
         console.log('Challenge accepted!');
-        KnownToken.findOneAndUpdate(
-          {
-            token,
-          },
-          {
-            token,
-          },
-          {
-            upsert: true,
-          },
-        );
         res.send(challenge);
       } else {
         console.log('No challenge token');
         res.sendStatus(400).send('No challenge token');
-      }
-      break;
-    case 'app_uninstalled':
-      console.log('Clearing app tokens...');
-      if (token) {
-        console.log('Token found in payload');
-        KnownToken.findOneAndDelete({
-          token,
-        });
-        res.send(`Uninstalled app from workspace ${teamId}`);
-      } else {
-        console.log('No app token');
-        res.sendStatus(400).send('No app token');
       }
       break;
     default:
@@ -63,20 +36,13 @@ router.post('/slash', (req, res) => {
     case 'mafiascummod':
     default:
       console.log('Handling /mafiascummod...');
-      KnownToken.findOne({ token }, tokenErr => {
-        if (tokenErr) {
-          console.log('Invalid app token');
-          res.sendStatus(400).send('Invalid app token');
+      Game.findOne({ channelId, modUserId: userId, workspaceToken: token }, err => {
+        if (err) {
+          console.log('Game not found');
+          res.sendStatus(400).send('Game not found');
         } else {
-          Game.findOne({ channelId, modUserId: userId, workspaceToken: token }, err => {
-            if (err) {
-              console.log('Game not found');
-              res.sendStatus(400).send('Game not found');
-            } else {
-              console.log(responseUri, text);
-              res.send('OK');
-            }
-          });
+          console.log(responseUri, text);
+          res.send('OK');
         }
       });
   }
